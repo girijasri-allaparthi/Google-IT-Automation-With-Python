2d193eb7de1ef256ce983aae02d41f2dea99878a
Girijasri <girijasri.allaparthi@gmail.com>
2024-03-12 16:20:13 +0000

    This is the program to change the number to positive

---
+++ make_positive.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#### This programs turns the number into positive#####
+def make_positive(number):
+
+        if number < 0:
+
+            result = number* -1
+
+        else:
+
+            result = number
+
+        return result
+
+
+
+make_positive(number)
